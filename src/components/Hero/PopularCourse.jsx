e744da5be50b9316157e6cef8b4ab3900fed5710
Abhiram <abhiramsuresh55@gmail.com>
2024-09-06 04:52:46 +0530

    popular course final completed

--- src/components/Hero/PopularCourse.jsx
+++ src/components/Hero/PopularCourse.jsx
@@ -1,40 +1,51 @@
+import { BookmarkMinus } from 'lucide-react';
 
 const PopularCourse = () => {
   return (
-    <div className="bg-zinc-100 w-full h-80">
-        <div className="pt-4 flex gap-20">
-          <h1 className=" pl-10 mr-10 font-bold">Popular Course</h1>
-          <p className="ml-96 text-[#7455F7] font-medium text-sm cursor-pointer">View All</p>
+    <div className="bg-zinc-100 w-[1010px] h-[420px]">
+        <div className="flex justify-between ">
+          <h1 className="pl-10 font-semibold text-xl text-[#222F30]">Popular Course</h1>
+          <p className=" text-[#7455F7] font-medium text-sm cursor-pointer mr-10">View All</p>
         </div>
         
-        <div className="flex flex-row gap-3 mt-5">
-        <div className=" ml-10 h-64 w-56 bg-white rounded-lg shadow-sm">
+        <div className="flex flex-row gap-5 mt-5">
+        <div className=" ml-10 h-[350px] w-[300px] bg-white rounded-2xl shadow-sm">
           <div className="">
-          <div className="h-36 w-52 rounded-lg mx-2 my-2 bg-orange-500"></div>
-          <div className="absolute text-[#7455F7] translate-x-36 -translate-y-12"><span className="px-1 py-1 rounded-2xl bg-white">$400</span></div>
+          <div className="h-48 w-[268px] rounded-2xl mx-4 my-4 bg-orange-500"></div>
+          <div className="absolute text-[#7455F7] translate-x-56 -translate-y-14"><span className="px-1 py-1 rounded-2xl bg-white">$400</span></div>
           </div>
-          <p className="text-sm font-semibold text-[#7455F7] ml-2 ">DESIGN</p>
-          <h3 className="text-base font-semibold  ml-2 ">Create 3D With <br/>Blender</h3>
-          <p className="text-xs ml-2 ">16 Lessons • 48 Hours</p>
+          <div className='flex justify-between'>
+          <p className="text-sm font-semibold text-[#7455F7] ml-4 ">DESIGN</p>
+          <BookmarkMinus size={40} color="#7455F7" strokeWidth={3} absoluteStrokeWidth className='px-2 py-2 rounded-full bg-[#EAE6FE] absolute translate-x-60' />
+          </div>
+          <h3 className="text-lg font-semibold  ml-4 ">Create 3D With <br/>Blender</h3>
+          <p className="text-base ml-4 mt-2 ">16 Lessons • 48 Hours</p>
         </div>
-        <div className=" h-64 w-56 bg-white rounded-lg shadow-sm">
-        <div>
-          <div className="h-36 w-52 rounded-lg mx-2 my-2 bg-orange-500"></div>
-          <div className="absolute text-[#7455F7] translate-x-36 -translate-y-12"><span className="px-1 py-1 rounded-2xl bg-white">$100</span></div>
+
+        <div className=" h-[350px] w-[300px] bg-white rounded-2xl shadow-sm">
+          <div className="">
+          <div className="h-48 w-[268px] rounded-2xl mx-4 my-4 bg-orange-500"></div>
+          <div className="absolute text-[#7455F7] translate-x-56 -translate-y-14"><span className="px-1 py-1 rounded-2xl bg-white">$100</span></div>
           </div>
-          <p className="text-sm font-semibold text-[#7455F7] ml-2 ">BUSINESS</p>
-          <h3 className="text-base font-semibold  ml-2 ">Digital Marketing</h3><br/>
-          <p className="text-xs ml-2 ">30 Lessons • 48 Hours</p>
+          <div className='flex justify-between'>
+          <p className="text-sm font-semibold text-[#7455F7] ml-4 ">BUSINESS</p>
+          <BookmarkMinus size={40} color="#7455F7" strokeWidth={3} absoluteStrokeWidth className='px-2 py-2 rounded-full bg-[#EAE6FE] absolute translate-x-60' />
+          </div>
+          <h3 className="text-lg font-semibold  ml-4 ">Digital Marketing</h3>
+          <p className="text-base ml-4 mt-2 ">30 Lessons • 48 Hours</p>
         </div>
-        
-        <div className=" h-64 w-56 bg-white rounded-lg shadow-sm">
-        <div>
-          <div className="h-36 w-52 rounded-lg mx-2 my-2 bg-orange-500"></div>
-          <div className="absolute text-[#7455F7] translate-x-36 -translate-y-12"><span className="px-1 py-1 rounded-2xl bg-white">$100</span></div>
+
+        <div className="h-[350px] w-[300px] bg-white rounded-2xl shadow-sm">
+          <div className="">
+          <div className="h-48 w-[268px] rounded-2xl mx-4 my-4 bg-orange-500"></div>
+          <div className="absolute text-[#7455F7] translate-x-56 -translate-y-14"><span className="px-1 py-1 rounded-2xl bg-white">$100</span></div>
           </div>
-          <p className="text-sm font-semibold text-[#7455F7] ml-2 ">CODE</p>
-          <h3 className="text-base font-semibold  ml-2 ">Slicing Ul Design <br/>With Tailwind</h3>
-          <p className="text-xs ml-2 ">30 Lessons • 48 Hours</p>
+          <div className='flex justify-between'>
+          <p className="text-sm font-semibold text-[#7455F7] ml-4 ">CODE</p>
+          <BookmarkMinus size={40} color="#7455F7" strokeWidth={3} absoluteStrokeWidth className='px-2 py-2 rounded-full bg-[#EAE6FE] absolute translate-x-60' />
+          </div>
+          <h3 className="text-lg font-semibold  ml-4 ">Slicing Ul Design <br/>With Tailwind</h3>
+          <p className="text-base ml-4 mt-2 ">30 Lessons • 48 Hours</p>
         </div>
         </div>
     </div>
